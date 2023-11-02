57784268ce1bec894f2d7439dc6edb29d352d31f
wkdehf217 <wkdehfdl17@naver.com>
2023-11-02 15:56:27 +0900

    Update, Delete 구현 ( 아까보단 덜 헷갈리네 )

--- src/main/java/com/memo/memo/controller/MemoController.java
+++ src/main/java/com/memo/memo/controller/MemoController.java
@@ -36,11 +36,40 @@ public class MemoController {
 
     // 조회
     @GetMapping("/memos")
-    public List<MemoResponseDto> getMemos(){ // List인 이유는 메모가 하나일리 없어서
+    public List<MemoResponseDto> getMemos() { // List인 이유는 메모가 하나일리 없어서
         // Map To List
         List<MemoResponseDto> responseList = memoList.values().stream() // stream : 하나씩 for문처럼 돌려줌
                 .map(MemoResponseDto::new).toList();                    // map : 변환
 
         return responseList;
+    }
+
+    // Json 으로 받아온다고하면 RequestBody 눈치
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            // 메모 수정
+            memo.update(requestDto);
+            return memo.getId(); // = id
+        } else {
+            throw new IllegalArgumentException("존재하지 않는 메모입니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) { // 지우는거니까 RequestBody 필요 x
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 가져오기
+            memoList.remove(id);
+
+            return id;
+        } else {
+            throw new IllegalArgumentException("존재하지 않는 메모입니다.");
+        }
     }
 }
